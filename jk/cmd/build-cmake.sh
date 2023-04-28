721500901771e1426774c75fc0a8abe51df5ba6e
yul <aymeric.guth@protonmail.com>
2023-04-28 22:42:44 +0200

    Clean

--- jk/cmd/build-cmake.sh
+++
@@ -1,9 +0,0 @@
-cmake \
-	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-	-DCMAKE_INSTALL_PREFIX="$WORKSPACE"/usr \
-	-DCMAKE_BUILD_TYPE=Debug \
-	-S . \
-	-B ./build \
-	-G Ninja &&
-	ninja -C ./build &&
-	ninja -C ./build install
--- project-env-valid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-[ -z "$WORKSPACE" ] && exit 1
-[ -z "$PROJECT_NAME" ] && exit 1
-[ "$PWD" != "$WORKSPACE" ] && exit 1
-exit 0
