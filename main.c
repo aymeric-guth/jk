a6dc2b9a5e3ed48a166cdd6679d7adad66ee3567
yul <aymeric.guth@protonmail.com>
2023-05-06 22:37:32 +0200

    Update main thread, io handler thread

--- main.c
+++ main.c
@@ -15,6 +15,10 @@
 
 #include "fifo.h"
 
+#define QUEUE_SIZE 10
+// #define DEBUG
+
+
 int running = 1;
 
 
@@ -23,26 +27,22 @@ void sig_handler(int signum)
     running = 0;
 }
 
-int kbhit()
-{
-    struct timeval tv;
-    fd_set fds;
-    tv.tv_sec = 0;
-    tv.tv_usec = 10000;
-    FD_ZERO(&fds);
-    FD_SET(STDIN_FILENO, &fds);
-    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
-    return FD_ISSET(STDIN_FILENO, &fds);
-}
-
 void *kb_event(void *arg)
 {
     Q *q = (Q *) arg;
+    struct timeval tv;
+    fd_set fds;
+
+    tv.tv_sec = 0;
+    tv.tv_usec = 10000;
 
     while (running) {
-        int ret = kbhit();
+        FD_ZERO(&fds);
+        FD_SET(STDIN_FILENO, &fds);
+        select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+        int ret = FD_ISSET(STDIN_FILENO, &fds);
 #ifdef DEBUG
-        fprintf(stdout, "kbhit()=%d\n", ret);
+        fprintf(stdout, "kb_event | ret=%d\n", ret);
 #endif
         if (ret < 0) {
             return NULL;
@@ -52,23 +52,12 @@ void *kb_event(void *arg)
 
         int c = getchar();
 #ifdef DEBUG
-        fprintf(stdout, "getchar()=%d\n", ret);
+        fprintf(stdout, "kb_event | getchar()=%d\n", ret);
 #endif
         if (c == EOF) {
             return NULL;
         }
         Q_put(q, c);
-
-        /* if (c == EOF) { */
-        /*     return NULL; */
-        /* } else if (c == 0) { */
-        /*     ; */
-        /* } else if (c >= 32 && c <= 126) { */
-        /*     fprintf(stdout, "\e[0;32m%d\n", c); */
-        /* } else { */
-        /*     fprintf(stdout, "\e[0;31m%d\n", c); */
-        /* } */
-        /* fprintf(stdout, "\e[0m"); */
     }
     return NULL;
 }
@@ -91,10 +80,34 @@ int main(int argc, const char **argv)
     newt.c_cc[VMIN] = 1;
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
+    int data[QUEUE_SIZE] = {0};
+    Q q = { .head = 0, .tail = 0, .size = QUEUE_SIZE, .data = data};
+
     // io thread init
     pthread_t io_thread;
-    pthread_create(&io_thread, NULL, kb_event, NULL);
-    pthread_join(io_thread, NULL);
+    pthread_create(&io_thread, NULL, kb_event, &q);
+
+    while (running) {
+        int c = 0;
+        int rc = Q_get(&q, &c);
+#ifdef DEBUG
+        fprintf(stdout, "main | Q_get()=%d\n", rc);
+#endif
+        if (rc < 0) {
+            continue;
+        }
+
+        if (c == EOF) {
+            running = 0;
+        } else if (c == 0) {
+            ;
+        } else if (c >= 32 && c <= 126) {
+            fprintf(stdout, "\e[0;32m%d\n", c);
+        } else {
+            fprintf(stdout, "\e[0;31m%d\n", c);
+        }
+        fprintf(stdout, "\e[0m");
+    }
 
     // main processing loop
     fprintf(stdout, "end io_thread\n");
