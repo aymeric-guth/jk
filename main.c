2f611515f1bcac94180aca5f149c2f80e6010dac
yul <aymeric.guth@protonmail.com>
2023-05-06 20:55:42 +0200

    Add termios based non blocking kb input

--- main.c
+++ main.c
@@ -10,65 +10,104 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <fcntl.h>
 
+#include "fifo.h"
 
-int TERMX;
-int TERMY;
 int running = 1;
 
-typedef struct {
-    float x, y;
-} Vec2;
-
-void get_term_size(Vec2 *v)
-{
-    // runtime terminal size
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    v->x = (float)w.ws_col * 0.5f;
-    v->y = (float)w.ws_row;
-    TERMX = (int) v->x;
-    TERMY = (int) v->y;
-}
 
 void sig_handler(int signum)
 {
     running = 0;
 }
 
-int main()
+int kbhit()
 {
-    running = 1;
-    signal(SIGINT, sig_handler);
-    Vec2 term;
-    get_term_size(&term);
-    int termx = TERMX;
-    int termy = TERMY;
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 10000;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
 
-    if (termx <= 0 || termy <= 0)
-        return -1;
-
-    // clear screen
-    printf("\033[2J");
-    // hide cursor
-    printf("\e[?25l");
-    // tty raw mode, non buffered io
-    struct termios mode;
-    tcgetattr(0, &mode);
-    mode.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(0, TCSANOW, &mode);
-
-    // io thread init
-    /* pthread_t _kb_input; */
-    /* Q q = { .head = 0, .tail = 0, .size = QUEUE_SIZE, .data = qp}; */
-    /* pthread_create(&_kb_input, NULL, kb_input, &q); */
+void *kb_event(void *arg)
+{
+    Q *q = (Q *) arg;
 
     while (running) {
-        int c = getchar();
-        printf("%d\n", c);
-    }
+        int ret = kbhit();
+#ifdef DEBUG
+        fprintf(stdout, "kbhit()=%d\n", ret);
+#endif
+        if (ret < 0) {
+            return NULL;
+        } else if (ret == 0) {
+            continue;
+        }
 
+        int c = getchar();
+#ifdef DEBUG
+        fprintf(stdout, "getchar()=%d\n", ret);
+#endif
+        if (c == EOF) {
+            return NULL;
+        }
+        Q_put(q, c);
+
+        /* if (c == EOF) { */
+        /*     return NULL; */
+        /* } else if (c == 0) { */
+        /*     ; */
+        /* } else if (c >= 32 && c <= 126) { */
+        /*     fprintf(stdout, "\e[0;32m%d\n", c); */
+        /* } else { */
+        /*     fprintf(stdout, "\e[0;31m%d\n", c); */
+        /* } */
+        /* fprintf(stdout, "\e[0m"); */
+    }
+    return NULL;
+}
+
+
+int main(int argc, const char **argv)
+{
+    signal(SIGINT, sig_handler);
+
+    // clear screen
+    fprintf(stdout, "\033[2J");
+    // hide cursor
+    fprintf(stdout, "\033[?25l");
+
+    // tty raw mode, non buffered io
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    memcpy(&newt, &oldt, sizeof(struct termios));
+    newt.c_lflag &= ~(ICANON | ECHO);
+    newt.c_cc[VMIN] = 1;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // io thread init
+    pthread_t io_thread;
+    pthread_create(&io_thread, NULL, kb_event, NULL);
+    pthread_join(io_thread, NULL);
+
+    // main processing loop
+    fprintf(stdout, "end io_thread\n");
+
+    // terminal cleanup
+    // restore terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    // end color
+    fprintf(stdout, "\033[0m");
     // show cursor
-    printf("\e[?25h");
+    fprintf(stdout, "\033[?25h");
+    // restore screen
+    fprintf(stdout, "\033[?47l");
+
     return 0;
 }
